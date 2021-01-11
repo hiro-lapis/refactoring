baa193bfcb1efe235060088054b492348d144020
hiro-lapis <coolish555ddddd@gmail.com>
2021-01-11 19:18:08 +0900

    一時変数のインライン化

--- statement.js
+++ statement.js
@@ -11,51 +11,55 @@ function statement(invoice, plays) {
             minimumFractionDigits: 2,
         }).format;
     for (const perf of invoice.performances) {
-        const play = playFor(perf);
-        // const play = plays[perf.playID];
-        let thisAmount = aMountFor(perf, play);
+        let thisAmount = aMountFor(perf, playFor(perf));
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 喜劇の時は10人につき,さらにポイント加算
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
         // 注文の内訳を出力
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats) \n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats) \n`;
         totalAmount += thisAmount;
         result += `Amount owed is ${format(totalAmount / 100)}`;
         result += `You earned ${volumeCredits} credits \n`;
         return result;
     }
 
-      /**
-       * playe内にplayIDに応じた値を返却
-       * @param {*} aPerformance
-       */
+    /**
+     * playe内にplayIDに応じた値を返却
+     * @param {*} aPerformance
+     */
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
+
+    /**
+     *
+     * @param {*} aPerformance
+     * @param {*} play
+     */
+    function aMountFor(aPerformance, play) {
+        let result = 0;
+            switch (play.type) {
+                case "tragedy":
+                    result = 40000;
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (aPerformance.audience > 20) {
+                        result += 1000 + 50 * (aPerformance.audience - 20);
+                    }
+                    result += 300 * aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`unknown type: ${play.type}`);
+            }
+        return result;
+    }
 }
 
-function aMountFor(aPerformance, play) {
-    let result = 0;
-        switch (play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 1000 + 50 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-    return result;
-}
 
 module.exports = statement;
